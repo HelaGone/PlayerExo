69cce7e704fd584f045cc99e312816d19b601f0d
helagone <luah613@gmail.com>
2018-02-23 19:03:15 -0600

    first commit

---
+++ app/src/main/java/com/helagone/airelibre/service/PlaybackStatus.java
@@ -0,0 +1,16 @@
+package com.helagone.airelibre.service;
+
+/**
+ * Created by programacion2 on 2/23/18.
+ */
+
+public class PlaybackStatus {
+
+    public static final String IDLE = "PlaybackStatus_IDLE";
+    public static final String LOADING = "PlaybackStatus_LOADING";
+    public static final String PLAYING = "PlaybackStatus_PLAYING";
+    public static final String PAUSED = "PlaybackStatus_PAUSED";
+    public static final String STOPPED = "PlaybackStatus_STOPPED";
+    public static final String ERROR = "PlaybackStatus_ERROR";
+
+}
